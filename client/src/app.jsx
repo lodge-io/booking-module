971f135a438bb1a06281bfd01a3024fb579fde45
carl12 <carl17r@gmail.com>
2019-04-02 09:14:15 -0700

    Allow calendar to cycle between months

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -22,26 +22,45 @@ function numToDay(num) {
   return DAY_NAMES[num];
 }
 
+console.log(Date, 'is the date object');
+console.log(Set, ' is a set');
+console.log(Math, ' is math');
+console.log(Map, ' is map');
+
+
 function getLastMonthDate(month, year = 2019) {
   return (new Date(year, month + 1, 0)).getDate();
 }
-console.log(Date);
-function numToMonth(num) {
+console.log('asdf')
+function numToMonth(num) { 
   return MONTH_NAMES[num];
 }
 
 function getMonthStartWeekday(month, year = 2019) {
-  return new Date(year, month).getDate();
+  return new Date(year, month).getDay();
 }
 
 const CalendarBox = styled.div`
   background: transparent;
-  border: 2px solid palevioletred;
   color: palevioletred;
-  // margin: 0 1em;
   padding: 0.25em 0.25em;
   text-align: center;
 `
+const TableD = styled.td`
+  border: 1px solid green;
+  width: 42px; height:42px; 
+`
+
+const ArrowSpan = styled.span`
+  flex-grow: 1;
+  text-align: center;
+`
+const MonthSpan = styled.span`
+  flex-grow: 8;
+  text-align: center;
+`
+
+
 const Head = styled.th`
   width: 14%;
   background-color:green;
@@ -50,47 +69,78 @@ const Table = styled.table`
   background-color:orange;
   width:100%;
   padding:0px;
-
+  border-collapse: collapse;
 `
-
+const TopRow = styled.span`
+  display: flex;
+  flex-direction: row;
+`
 const TableHolder = styled.div`
   background-color:blue;
-  width: 100%;
+  width: 300px;
+  padding: 20px;
+
 `;
-const Date = styled.div`
+// const Date = styled.div`
   
-`;
+// `;
 
 class Asdf extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { month: 3, year: 2019};
   }
+
+  nextMonth(){
+    let month = this.state.month + 1;
+    let year = this.state.year;
+    if (month === 12) {
+      month = 0;
+      year = this.state.year + 1;
+    }
+    this.setState({ month, year });
+  }
+
+  lastMonth(){
+    let month = this.state.month - 1;
+    let year = this.state.year;
+    if (month === -1) {
+      month = 11;
+      year = this.state.year - 1;
+    }
+    this.setState({ month, year });
+  }
+
   render(){
-    const year = 2019;
-    const monthIn = 11;
-    let month = MONTH_NAMES[monthIn];
+    const year = this.state.year;
+    let month = MONTH_NAMES[this.state.month];
     let date = 1;
-    let lastDay = 32;//getLastMonthDate(monthIn, year);
-    let firstDay = 4;//getMonthStartWeekday(monthIn, year);
+    let lastDay = getLastMonthDate(this.state.month, year);
+    console.log(this.state.month, year);
+    let firstDay = getMonthStartWeekday(this.state.month, year);
+    console.log(firstDay);
     let table = [];
     
     while(date <= lastDay) {
       let week = [];
       for (let i = 0; i < 7; i++) {
         if ((date === 1 && i < firstDay) || date > lastDay) {
-          week.push(<td></td>);
+          week.push(<TableD></TableD>);
         } else {
           let myDate = date;
-          week.push(<td><CalendarBox onMouseOver={()=>console.log(myDate)}>{myDate}</CalendarBox></td>)
+          week.push(<TableD><CalendarBox onMouseOver={()=>console.log(myDate)}>{myDate}</CalendarBox></TableD>)
           date++;
         }
       }
       table.push(week);
     }
-    console.log(table);
     return (
       <TableHolder> 
-        {month}
+        <TopRow>
+          <ArrowSpan onClick={() => this.lastMonth()}>&lt;</ArrowSpan>
+          <MonthSpan>{month}</MonthSpan>
+          <ArrowSpan onClick={() => this.nextMonth()}>&gt;</ArrowSpan>
+        </TopRow>
         <Table>
           <tr>
             {SHORT_DAY_NAMES.map((val) =>  <Head>{val}</Head> )}
