b3b16e5b2ce8b0ce6105de44dc32a545b02c328d
Carl Rodriguez <carl17r@gmail.com>
2019-04-02 21:04:32 -0700

    Revert "Enzyme"

--- client/src/__tests__/Hello.spec.jsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import { shallow } from 'enzyme';
-import App from '../App2.jsx';
-
-it('renders in the dom', () => {
-  const res = shallow(<App />).contains(<div>asdf</div>);
-  expect(res).toBeTruthy();
-});
