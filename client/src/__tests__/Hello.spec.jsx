d47585e36921bf66d99280bab8a81a7e9cf8d106
carl12 <carl17r@gmail.com>
2019-04-02 20:48:41 -0700

    Move tested file to be distinct from master

--- client/src/__tests__/Hello.spec.jsx
+++ client/src/__tests__/Hello.spec.jsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { shallow } from 'enzyme';
-import App from '../app.jsx';
+import App from '../App2.jsx';
 
 it('renders in the dom', () => {
   const res = shallow(<App />).contains(<div>asdf</div>);
