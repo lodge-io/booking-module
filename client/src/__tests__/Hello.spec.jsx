42d1f29c405a8bcf624aa3d188e966731a20607d
carl12 <carl17r@gmail.com>
2019-04-02 21:16:29 -0700

    Update deleted files to prevent deletion in merge

--- client/src/__tests__/Hello.spec.jsx
+++ client/src/__tests__/Hello.spec.jsx
@@ -4,5 +4,5 @@ import App from '../App2.jsx';
 
 it('renders in the dom', () => {
   const res = shallow(<App />).contains(<div>asdf</div>);
-  expect(res).toBeTruthy();
+  expect(res).toBeTruthy(); 
 });
--- jest.config.js
+++ jest.config.js
@@ -14,7 +14,7 @@ module.exports = {
   // The paths to modules that run some code to configure or set up the testing environment before each test
   setupFiles: ['<rootDir>/enzyme.config.js'],
 
-  // The test environment that will be used for testing
+  // The test environment that will be used for testing 
   testEnvironment: 'jsdom',
 
   // The glob patterns Jest uses to detect test files
